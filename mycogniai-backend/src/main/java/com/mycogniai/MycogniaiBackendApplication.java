523f23d94d463360ce3ee427dd80d2aad8f55fe8
Adedayo-Data <theophilusdayo@outlook.com>
2025-07-12 14:34:26 +0100

    Pom File updated

---
+++ mycogniai-backend/src/main/java/com/mycogniai/MycogniaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mycogniai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycogniaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycogniaiBackendApplication.class, args);
+	}
+
+}
---
+++ mycogniai-backend/src/test/java/com/mycogniai/MycogniaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycogniai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycogniaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
